56d0ee84b98a963a026fd6e24c3bcd627de6643d
olaoluwiv <ayidinao@gmail.com>
2024-01-16 02:55:02 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add - add two integer
+ * @a: first unit
+ * @b: second unit
+ * Return: addtion
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+
+}
+
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - mode of two integer
+ * @a: first unit
+ * @b: second unit
+ * Return: Return answer
+ */
+
+int mod(int a, int b)
+{
+	return (a & b);
+
+}
+
